971ec2e78a347b01aae7450b76a08cb38ce1dc3f
ap1 <anjul.patney@gmail.com>
2014-07-19 22:08:49 -0700

    first example is up!

---
+++ util.js
@@ -0,0 +1,53 @@
+function InitWebGL(gl_element_id)
+{
+  var glwin = document.getElementById(gl_element_id);
+
+  try
+  {
+    gl = glwin.getContext('experimental-webgl');
+    gl.viewportWidth  = glwin.width;
+    gl.viewportHeight = glwin.height;
+    gl.clearColor(0.0, 0.0, 0.2, 1.0);
+    gl.enable(gl.DEPTH_TEST);
+  }  
+  catch(e)
+  {
+    alert("init error!");
+  }
+
+  return gl;
+}
+
+
+function ShaderFromString(gl, str, type)
+{
+  //console.log(str);
+  var shader = gl.createShader(type);
+  gl.shaderSource(shader, str);
+  gl.compileShader(shader);
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+  {
+    throw gl.getShaderInfoLog(shader);
+    //alert("shader create error!");
+  }
+  return shader;
+}
+
+function CreateProgram(gl, vs, fs)
+{
+  var program = gl.createProgram();
+  console.log('Creating VS');
+  var vshader = ShaderFromString(gl, vs, gl.VERTEX_SHADER);
+  console.log('Creating FS');
+  var fshader = ShaderFromString(gl, fs, gl.FRAGMENT_SHADER);
+  gl.attachShader(program, vshader);
+  gl.attachShader(program, fshader);
+  gl.linkProgram(program);
+  if (!gl.getProgramParameter(program, gl.LINK_STATUS))
+  {
+    alert("shader init error!");
+    //throw gl.getProgramInfoLog(program);
+  }
+  return program;
+}
+
